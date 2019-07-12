b210055535bea940c22e2fb089dddb61e9fe043c
Tommy <haithienld@gmail.com>
2019-07-12 22:39:06 +0900

    add tommy

---
+++ tommy-mnist.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+import os
+
+import tensorflow as tf
+import random
+import matplotlib.pyplot as plt
+
+hello = tf.constant("Hello, Tensorflow")
+print(hello)
+
+
+#cost / lost function 
